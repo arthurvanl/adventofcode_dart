0fe51da76d0a70bde23064c21f1cf850bf855300
arthurvanl <arthurvanleeuwen@hotmail.nl>
2022-06-18 12:17:41 +0200

    Added test dart file & added day1 part 2

--- lib/solutions/2021/day1.dart
+++ lib/solutions/2021/day1.dart
@@ -1,15 +1,20 @@
 import '../../utils/input.dart';
 
 void main() {
+  List<dynamic> lines = getLines();
+
+  part1(lines);
+  part2(lines);
+}
+
+List<dynamic> getLines() {
   List<dynamic> lines = readLinesAsString(1);
 
   // removing falsey values
   lines.removeWhere((x) => ["", null, false, 0].contains(x));
-  // parsing values to int
-  lines = lines.map((x) => int.parse(x)).toList();
 
-  part1(lines);
-  part2(lines);
+  // parsing values to int
+  return lines.map((x) => int.parse(x)).toList();
 }
 
 void part1(List<dynamic> lines) {
@@ -26,7 +31,16 @@ void part1(List<dynamic> lines) {
 }
 
 void part2(List<dynamic> lines) {
-  var test = lines[-1] || true;
+  int counter = -1;
+  int prev = -1;
 
-  print(test);
+  for (int i = 2; i < lines.length; i++) {
+    int curr = lines[i - 2] + lines[i - 1] + lines[i];
+
+    if (curr > prev) counter++;
+
+    prev = curr;
+  }
+
+  print('Answer part 2: $counter');
 }
---
+++ lib/test.dart
@@ -0,0 +1,8 @@
+import 'solutions/2021/day1.dart';
+
+void main() {
+  List<dynamic> lines = getLines();
+
+  part1(lines);
+  part2(lines);
+}
